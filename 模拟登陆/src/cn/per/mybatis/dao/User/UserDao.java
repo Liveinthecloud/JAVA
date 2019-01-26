bf01d2f0376268c7f02d6ea7a6814af80fecaa5a
Liveinthecloud <zv279901034@gmail.com>
2019-01-26 14:44:19 +0800

    2019/1/26

--- 模拟登陆/src/cn/per/mybatis/dao/User/UserDao.java
+++
@@ -1,7 +0,0 @@
-package cn.per.mybatis.dao.User;
-
-import cn.per.mybatis.model.User;
-
-public interface UserDao {
-    public User findUserByUserNameAndPasswordDao(User user);
-}
--- 模拟登陆/src/cn/per/mybatis/service/Service_interface/UserService.java
+++
@@ -1,7 +0,0 @@
-package cn.per.mybatis.service.Service_interface;
-
-import cn.per.mybatis.model.User;
-
-public interface UserService {
-    public User findUserByUserNameAndPassword(User user);
-}
