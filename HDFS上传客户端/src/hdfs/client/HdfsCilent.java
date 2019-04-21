93731d4b264f42d46d90238bf6b04da07c82987e
Liveinthecloud <41690109+Liveinthecloud@users.noreply.github.com>
2019-04-21 13:33:07 +0800

    2019/4/21

--- HDFS上传客户端/src/hdfs/client/HdfsCilent.java
+++ HDFS上传客户端/src/hdfs/client/HdfsCilent.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.BufferedReader;
@@ -14,12 +16,12 @@ import java.io.InputStreamReader;
 import java.net.URI;
 import java.nio.Buffer;
 
-
 public class HdfsCilent {
    static FileSystem fs=null;
    static{
        try {
-           fs = FileSystem.get(new URI("hdfs://master:9000/"), new Configuration(), "root");
+           Configuration conf = new Configuration();
+           fs = FileSystem.get(new URI("hdfs://master:9000/"), conf, "root");
        }catch (Exception e){
            e.getLocalizedMessage();
        }
@@ -28,9 +30,9 @@ public class HdfsCilent {
     /**
      * 下载文件
      */
-
+    @Test
     public void download()throws Exception {
-        fs.copyToLocalFile(new Path("/test/jdk-8.tar.gz"),new Path("f:/"));
+        fs.copyToLocalFile(new Path("/jdk-8u201-linux-x64.tar.gz"),new Path("I:/"));
         fs.close();
         System.out.println("下载完成");
     }
@@ -39,7 +41,7 @@ public class HdfsCilent {
     * */
     @Test
     public void readData() throws Exception{
-        FSDataInputStream open = fs.open(new Path("/wordcount/output/res.dat"));
+        FSDataInputStream open = fs.open(new Path("/rating.json"));
         /*byte[] buf = new byte[1024];
         open.read(buf);
         System.out.println(new String(buf));*/
@@ -55,10 +57,11 @@ public class HdfsCilent {
     /**
      *向hdfs中写数据
      */
+    @Test
     public void writeData() throws Exception{
-        FSDataOutputStream out = fs.create(new Path("/test/write.jpg"), true);
+        FSDataOutputStream out = fs.create(new Path("/rating.json"), true);
 //        C:\Users\zengwei\Desktop\sun.jpg
-        FileInputStream in = new FileInputStream("C:\\Users\\zengwei\\Desktop\\sun.jpg");
+        FileInputStream in = new FileInputStream("C:\\Users\\zengwei\\Desktop\\rating.json");
         byte[] buf = new byte[1024];
         int read=0;
         while((read=in.read(buf))!=-1){
